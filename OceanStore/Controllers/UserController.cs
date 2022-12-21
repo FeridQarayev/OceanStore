b0bc881aea0ea6a907ec129947d4c0ac6180df2a
FeridQarayev <tu7he6n9s@code.edu.az>
2022-12-22 00:09:55 +0400

    create user post method

--- OceanStore/Controllers/UserController.cs
+++ OceanStore/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using OceanStore.BusinessLayer.Managers;
 using OceanStore.DataAccesLayer.ViewModels;
 using System.Collections.Generic;
@@ -13,6 +14,31 @@ namespace OceanStore.Controllers
         {
             List<UserVM> users = await _userAppManager.GetAllUsers();
             return View(users);
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Roles = await _userAppManager.GetRoles();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RegisterVM registerVM, string role)
+        {
+            ViewBag.Roles = await _userAppManager.GetRoles();
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            IEnumerable<IdentityError> errors = await _userAppManager.CreateUser(registerVM, role);
+            if (errors!=null)
+            {
+                foreach (IdentityError error in errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
