b209f39c6edb63b4efcfe3df9864917e92828514
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-02 12:10:53 +0400

    add mail send method

--- OceanStore.BusinessLayer/Helpers/Helper.cs
+++ OceanStore.BusinessLayer/Helpers/Helper.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace OceanStore.BusinessLayer.Helpers
@@ -18,6 +16,22 @@ namespace OceanStore.BusinessLayer.Helpers
         public static bool CheckActive(bool active)
         {
             return active ? false : true;
+        }
+
+        public static async Task SendMailAsync(string messageSubject, string messageBody, string mailTo)
+        {
+            SmtpClient client = new SmtpClient("smtp.yandex.com", 587);
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("Farid.g@itbrains.edu.az", "kgzkbjlmhizqlgtp");
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            MailMessage message = new MailMessage("Farid.g@itbrains.edu.az", mailTo);
+            message.Subject = messageSubject;
+            message.Body = messageBody;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.IsBodyHtml = true;
+
+            await client.SendMailAsync(message);
         }
     }
 }
---
+++ OceanStore/Controllers/MailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OceanStore.Controllers
+{
+    public class MailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
