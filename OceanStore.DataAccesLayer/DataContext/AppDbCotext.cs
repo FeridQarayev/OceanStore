4078aee08a52fde31ab4fe2ddc61f339c50bda66
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-04 12:41:07 +0400

    add commit appdb context

--- OceanStore.DataAccesLayer/DataContext/AppDbCotext.cs
+++ OceanStore.DataAccesLayer/DataContext/AppDbCotext.cs
@@ -19,4 +19,14 @@ namespace OceanStore.DataAccesLayer.DataContext
         public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
     }
+    //class DataContextFactory : IDesignTimeDbContextFactory<AppDbCotext>
+    //{
+    //    public AppDbCotext CreateDbContext(string[] args)
+    //    {
+    //        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+    //        var optionBuilder = new DbContextOptionsBuilder<AppDbCotext>();
+    //        optionBuilder.UseSqlServer(configuration["ConnectionStrings:Context"]);
+    //        return new AppDbCotext(optionBuilder.Options);
+    //    }
+    //}
 }
