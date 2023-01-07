0062c5367e656d9ae31f76b6c76fcbfa37cae766
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-07 12:18:06 +0400

    Add Incomes Controller

---
+++ OceanStore/Controllers/IncomesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OceanStore.Controllers
+{
+    public class IncomesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
