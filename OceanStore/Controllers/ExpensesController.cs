fd5816740ffcd372b12d51076c34e6c20c03e075
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-12 23:27:08 +0400

    check employee count expenses controller

--- OceanStore/Controllers/ExpensesController.cs
+++ OceanStore/Controllers/ExpensesController.cs
@@ -13,15 +13,18 @@ namespace OceanStore.Controllers
     {
         #region ctor
         private readonly AmmountManager _ammountManager;
-        public ExpensesController(AmmountManager ammountManager)
+        private readonly EmployeeManager _employeeManager;
+        public ExpensesController(AmmountManager ammountManager, EmployeeManager employeeManager)
         {
             _ammountManager = ammountManager;
+            _employeeManager = employeeManager;
         }
         #endregion
 
         #region Index
         public async Task<IActionResult> Index()
         {
+            ViewBag.EmployeeCount = (await _employeeManager.GetAllEmployee()).Count;
             List<Ammount> ammounts = await _ammountManager.ListAllExpenses();
             return View(ammounts);
         }
@@ -59,11 +62,13 @@ namespace OceanStore.Controllers
         #endregion
 
         #region EmployeeSalary
+        [Authorize(Roles = "SuperAdmin")]
         public async Task<IActionResult> EmployeeSalary()
         {
             double totalSalary = await _ammountManager.GetTotalEmployeeSalary();
             return Json(totalSalary);
         }
+        [Authorize(Roles = "SuperAdmin")]
         public async Task<IActionResult> PayEmployeeSalary()
         {
             PayEmployeeSalary data = await _ammountManager.PayEmployeeSalary(User.Identity.Name);
